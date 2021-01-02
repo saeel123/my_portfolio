1f7424c5fa8033c9450c07178ba4e8dd3096b494
saeel <saeelmacman123@gmail.com>
2021-01-03 00:02:53 +0530

    added projects page

--- src/App.js
+++ src/App.js
@@ -13,10 +13,11 @@ function App() {
       <Switch>
         <Route component={Home} path='/' exact />
         <Route component={Post} path='/post' />
+        <Route component={Project} path='/project' />
         {/* <Route component={About} path='/about' />
-        <Route component={SinglePost} path='/post/:slug' />
+        <Route component={SinglePost} path='/post/:slug' /> */}
        
-        <Route component={Project} path='/project' /> */}
+      
       </Switch>
     </BrowserRouter>
   );
